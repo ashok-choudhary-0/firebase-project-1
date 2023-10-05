d4f3f7acf8d057e4a54af22529f41b4ff5902704
ashok-choudhary-0 <ashok.kumar@innotechteam.in>
2023-10-05 10:26:57 +0530

    added serviceAccount.json in gitignore

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const port = process.env.port || 5000;
 const app = express();
 app.use(bodyParser.json())
 app.use("/user", userRoutes);
-
 app.listen(port, (req, res) => {
   console.log(`server is running on port ${port}`)
 })
